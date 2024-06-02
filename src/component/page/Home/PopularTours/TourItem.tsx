01325888c321336fe3a4cb651805bb43fdeb9531
havanduc20194510 <havanducdeptrai@gmail.com>
2024-06-02 15:19:10 +0700

    Fix tour card

--- src/component/page/Home/PopularTours/TourItem.tsx
+++ src/component/page/Home/PopularTours/TourItem.tsx
@@ -2,9 +2,12 @@
 "use client";
 
 import { View } from "@/component/ui/View";
+import { AverageRating } from "@/models/review/get";
 import { Tour } from "@/models/tour/get";
+import { getTourAverageRating } from "@/service/review";
 import { formatPrice } from "@/utils/price";
 import Link from "next/link";
+import { useState, useCallback, useEffect } from "react";
 
 type Props = {
   tour: Tour;
@@ -12,6 +15,21 @@ type Props = {
 };
 
 export default function TourItem({ tour, colSpan = "1" }: Props) {
+  const [averageRating, setAverageRating] = useState<AverageRating>();
+
+  const loadAverageRating = useCallback(async () => {
+    try {
+      const averageRatingRes = await getTourAverageRating(tour.tourId);
+      setAverageRating(averageRatingRes.data);
+    } catch {
+      //Do nothing
+    }
+  }, [tour.tourId]);
+
+  useEffect(() => {
+    loadAverageRating();
+  }, [loadAverageRating]);
+
   return (
     <Link
       href={`tours/${tour.tourId}`}
@@ -25,13 +43,24 @@ export default function TourItem({ tour, colSpan = "1" }: Props) {
       />
       <div className="relative w-full bg-black bg-opacity-40 backdrop-filter backdrop-blur-md p-3">
         <b className="text-emerald-500 text-lg">{tour.name}</b>
-        <div>
+        <div className="flex items-center gap-3">
           <span className="font-bold text-lg text-white">
             {formatPrice(tour.adultPrice)}
           </span>
-          <span className="text-emerald-500 font-bold text-xs ml-2">
-            Price stars from
-          </span>
+          <div className="flex items-center">
+            {!!averageRating?.averageRating &&
+              Array.from({ length: averageRating?.averageRating ?? 0 }).map(
+                (_, index) => (
+                  <img
+                    key={index}
+                    width={20}
+                    height={20}
+                    src="/icons/star.svg"
+                    alt=""
+                  />
+                )
+              )}
+          </div>
         </div>
       </div>
       <View totalView={tour.totalView} />
