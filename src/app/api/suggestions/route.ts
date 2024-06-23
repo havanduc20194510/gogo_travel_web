4f297602c7b782d3ba6a42d79343117b22e96bda
VanDucKun <havanducdeptrai@gmail.com>
2024-06-23 17:38:34 +0700

    change-key-2

--- src/app/api/suggestions/route.ts
+++ src/app/api/suggestions/route.ts
@@ -46,7 +46,7 @@ const GET = async (request: Request) => {
     textPrompt += ' Format dưới dạng tên địa điểm - mô tả nổi bật của địa điểm. Không có dấu ** xuất hiện';
 
     // Khởi tạo đối tượng OpenAI với API key
-    const openai = new OpenAI({apiKey:"sk-gotravelapp-Vw7xdD0dOol6uRD7tBZpT3BlbkFJszHwDD4XBLpdH851rN9k"});
+    const openai = new OpenAI({apiKey:"sk-travel-key-1-A6GehGc1loD24A0AEplDT3BlbkFJjEXEAzoAcEMLin9KgoEk"});
 
     try {
       // Gửi yêu cầu tới OpenAI API để tạo nội dung
