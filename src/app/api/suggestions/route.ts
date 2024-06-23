c31d694b74b3873d208bd8bb2f4eec5f0c814f51
VanDucKun <havanducdeptrai@gmail.com>
2024-06-23 17:36:58 +0700

    change-key-1

--- src/app/api/suggestions/route.ts
+++ src/app/api/suggestions/route.ts
@@ -11,8 +11,9 @@ const GET = async (request: Request) => {
     const location = searchParams.get('location');
     const time = searchParams.get('time');
     const activity = searchParams.get('activity');
+    
     // check params
-    if(!location) {
+    if (!location) {
       return new NextResponse(
         JSON.stringify({ error: 'Missing location parameter' }),
         {
@@ -21,7 +22,7 @@ const GET = async (request: Request) => {
       );
     }
 
-    if(!time) {
+    if (!time) {
       return new NextResponse(
         JSON.stringify({ error: 'Missing time parameter' }),
         {
@@ -30,7 +31,7 @@ const GET = async (request: Request) => {
       );
     }
 
-    if(!activity) {
+    if (!activity) {
       return new NextResponse(
         JSON.stringify({ error: 'Missing activity parameter' }),
         {
@@ -39,78 +40,84 @@ const GET = async (request: Request) => {
       );
     }
 
-     // Tạo text prompt từ các tham số
-    let textPrompt = `Gợi ý top 5 địa điểm vui chơi ở ${location} vào thời gian ${time}`;
+    // Tạo text prompt từ các tham số
+    let textPrompt = `Gợi ý top 4 địa điểm vui chơi ở ${location} vào thời gian ${time}`;
     if (activity) textPrompt += ` có hoạt động ${activity}.`;
-    textPrompt += 'Format dưới dạng Location - Description.';
+    textPrompt += ' Format dưới dạng tên địa điểm - mô tả nổi bật của địa điểm. Không có dấu ** xuất hiện';
 
     // Khởi tạo đối tượng OpenAI với API key
-    const openai = new OpenAI({apiKey:"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"});
+    const openai = new OpenAI({apiKey:"sk-gotravelapp-Vw7xdD0dOol6uRD7tBZpT3BlbkFJszHwDD4XBLpdH851rN9k"});
 
-  try {
-    // Gửi yêu cầu tới OpenAI API để tạo nội dung
-    const response = await openai.chat.completions.create({
-      model: 'gpt-4o',
-      messages: [{ role: 'user', content: textPrompt }],
-      temperature: 0,
-      max_tokens: 2000,
-    });
+    try {
+      // Gửi yêu cầu tới OpenAI API để tạo nội dung
+      const response = await openai.chat.completions.create({
+        model: 'gpt-4o',
+        messages: [{ role: 'user', content: textPrompt }],
+        temperature: 0,
+        max_tokens: 4000,
+      });
 
-    // Xử lý phản hồi từ OpenAI API
-    const [, ...entries] = JSON.stringify(
-      response.choices[0].message.content
-    ).split("\\n");
+      const content = response.choices[0].message.content;
+      const entries = content?.split('\n').filter(entry => entry.trim() !== '');
 
-    const destinations: DestinationItem[] = [];
+      const destinations: DestinationItem[] = [];
+      if (entries == null) {
+        return new NextResponse(
+            JSON.stringify({}),
+            {
+                  status: 200,
+            }
+        );
+      }
+      for (const entry of entries) {
+        const [locationWithNumber, description] = entry.split(' - ');
+        const location = locationWithNumber.substring(locationWithNumber.indexOf('.') + 2).trim();
 
-    for (const entry of entries) {
-      const [locationWithNumber, description] = entry.split(" - ");
-      const [, location] = locationWithNumber.split(".");
-
-      if (location) {
-        // Gửi yêu cầu tới OpenAI API để tạo hình ảnh
-        try {
-          const image = await openai.images.generate({
-            prompt: 'Ảnh thực tế của địa điểm ' + location,
-            n: 1,
-            size: '512x512',
-          });
-          destinations.push({
-            location,
-            description,
-            img: image.data[0].url,
-          });
-        }
-        catch(error){
-          console.log(error);
-          return new NextResponse(
-          JSON.stringify({ error: 'Lỗi khi call api generate ảnh.' }),
-          {
-            status: 500,
+        if (location) {
+          // Gửi yêu cầu tới OpenAI API để tạo hình ảnh
+          try {
+            const image = await openai.images.generate({
+              prompt: 'Ảnh thực tế của địa điểm ' + location,
+              n: 1,
+              size: '512x512',
+            });
+            destinations.push({
+              location,
+              description,
+              img: image.data[0].url,
+            });
           }
-          );
+          catch (error) {
+            console.log(error);
+            return new NextResponse(
+              JSON.stringify({ error: 'Lỗi khi call api generate ảnh.' }),
+              {
+                status: 500,
+              }
+            );
+          }
         }
       }
+
+      console.log(destinations);
+
+      // Gửi phản hồi về các điểm đến
+      return new NextResponse(JSON.stringify(destinations), {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+    } catch (error) {
+      console.log(error);
+      return new NextResponse(
+        JSON.stringify({ error: 'Lỗi khi call api địa điểm.' }),
+        {
+          status: 500,
+        }
+      );
     }
-
-    // Gửi phản hồi về các điểm đến
-    return new NextResponse(JSON.stringify(destinations), {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
   } catch (error) {
-    console.log(error);
-    return new NextResponse(
-      JSON.stringify({ error: 'Lỗi khi call api địa điểm.' }),
-      {
-        status: 500,
-      }
-    );
-  }
-  }
-  catch (error) {
     console.log(error);
     return new NextResponse(
       JSON.stringify({ error: 'An error occurred when get params.' }),
@@ -121,5 +128,3 @@ const GET = async (request: Request) => {
   }
 }
 export { GET }
-
-
